3c347f95ca35793396b3fa530118a564fb1f78c3
zwk <zwk123456@gmail.com>
2022-02-28 16:01:18 +0800

    fix：add kenzok8 small feeds

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-
+echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
